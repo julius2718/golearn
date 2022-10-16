baa21d9edc67a9601fca49d480ed805ca784e49e
julius2718 <yuhgon0928@gmail.com>
2022-10-16 14:09:00 +0200

    Test variable declaration

--- main.go
+++ main.go
@@ -5,5 +5,13 @@ import (
 )
 
 func main() {
+	var i int = 42
+	j := 23
+	var k int
+	k = 68
+
 	fmt.Println("Hello World!")
+	fmt.Printf("Number i is %v.", i)
+	fmt.Printf("Number j is %v.", j)
+	fmt.Printf("Number k is %v.", k)
 }
