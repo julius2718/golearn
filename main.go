cf49937027073571a071a83878c3b77f5737d14c
julius2718 <yuhgon0928@gmail.com>
2022-10-22 23:11:59 +0200

    Arrays and slices

--- main.go
+++ main.go
@@ -4,14 +4,33 @@ import (
 	"fmt"
 )
 
-func main() {
+func variables() {
 	var i int = 42
 	j := 23
 	var k int
 	k = 68
 
 	fmt.Println("Hello World!")
-	fmt.Printf("Number i is %v.", i)
-	fmt.Printf("Number j is %v.", j)
-	fmt.Printf("Number k is %v.", k)
+	fmt.Printf("Number i is %v.\n", i)
+	fmt.Printf("Number j is %v.\n", j)
+	fmt.Printf("Number k is %v.\n", k)
+}
+
+func arrays() {
+	grades := [3]int{93, 79, 84}
+	grades_b := [...]int{93, 43, 60}
+
+	grades_c := &grades
+
+	fmt.Printf("Grade: %v\n", grades)
+	fmt.Printf("Grade: %v\n", grades_b)
+
+	grades_c[1] = 99
+	fmt.Printf("Grade: %v\n", grades_c)
+	fmt.Printf("Original grade: %v\n", grades)
+}
+
+func main() {
+	variables()
+	arrays()
 }
